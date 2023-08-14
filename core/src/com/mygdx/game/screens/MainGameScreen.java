34cb6de76d82c40b954426559db23c7189fda0f4
KAYKOBAD HOSSAIN TANJIL <kaykobadhossaintanjil01@gmail.com>
2023-08-15 01:24:40 +0600

    Creating Game Screens

---
+++ core/src/com/mygdx/game/screens/MainGameScreen.java
@@ -0,0 +1,76 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class MainGameScreen implements Screen {
+    public static final float SPEED=120;
+    Texture img;
+    float x;
+    float y;
+    MyGdxGame game;
+    public MainGameScreen(MyGdxGame game){
+        this.game=game;
+    }
+    @Override
+    public void show() {
+        img =new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            y+=SPEED*Gdx.graphics.getDeltaTime();
+
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            y-=SPEED*Gdx.graphics.getDeltaTime();
+
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            x+=SPEED*Gdx.graphics.getDeltaTime();
+
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            x-=SPEED*Gdx.graphics.getDeltaTime();
+
+        }
+
+        game.batch.begin();
+        game.batch.draw(img,x,y);
+        game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
