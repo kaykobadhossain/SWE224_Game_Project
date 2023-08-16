c60efbf18aa30c4d0e5628c782e00ab6b2e24913
KAYKOBAD HOSSAIN TANJIL <kaykobadhossaintanjil01@gmail.com>
2023-08-16 16:43:39 +0600

    Spaceship Animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,7 +1,6 @@
 package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.screens.MainGameScreen;
 import com.mygdx.game.screens.MainMenuScreen;
 
 
--- core/src/com/mygdx/game/screens/MainGameScreen.java
+++ core/src/com/mygdx/game/screens/MainGameScreen.java
@@ -5,35 +5,49 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.mygdx.game.MyGdxGame;
 
 public class MainGameScreen implements Screen {
-    public static final float SPEED=120;
-    Texture img;
+    public static final float SPEED = 120;
+
+    public static final float SHIP_ANIMATION_SPEED = 0.5f;
+    public static final int SHIP_WIDTH_PIXEL = 17;
+    public static final int SHIP_HEIGHT_PIXEL = 32;
+    public static final int SHIP_WIDTH = SHIP_WIDTH_PIXEL * 3;
+    public static final int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL * 3;
+
+
+    Animation[] rolls;
     float x;
     float y;
+    int roll;
+    float stateTime;
+
     MyGdxGame game;
-    public MainGameScreen(MyGdxGame game){
-        this.game=game;
+
+    public MainGameScreen (MyGdxGame game) {
+        this.game = game;
+        y = 15;
+        x = (float) MyGdxGame.Width / 2 - (float) SHIP_WIDTH / 2;
+
+        roll = 2;
+        rolls = new Animation[5];
+
+        TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), SHIP_WIDTH_PIXEL, SHIP_HEIGHT_PIXEL);
+
+        rolls[roll] = new Animation(SHIP_ANIMATION_SPEED,rollSpriteSheet[0]);
     }
     @Override
     public void show() {
 
-        img =new Texture("badlogic.jpg");
+
     }
 
     @Override
     public void render(float delta) {
 
-
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            y+=SPEED*Gdx.graphics.getDeltaTime();
-
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            y-=SPEED*Gdx.graphics.getDeltaTime();
-
-        }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
             x+=SPEED*Gdx.graphics.getDeltaTime();
 
@@ -42,13 +56,16 @@ public class MainGameScreen implements Screen {
             x-=SPEED*Gdx.graphics.getDeltaTime();
 
         }
+        stateTime +=delta;
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.batch.draw(img,x,y);
+        game.batch.draw((TextureRegion) rolls[roll].getKeyFrame(stateTime, true), x, y, SHIP_WIDTH, SHIP_HEIGHT);
         game.batch.end();
 
     }
+
+
 
     @Override
     public void resize(int width, int height) {
