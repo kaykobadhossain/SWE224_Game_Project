2e7d05ab12fab1df4ca6004ed11527a2592da0eb
KAYKOBAD HOSSAIN TANJIL <kaykobadhossaintanjil01@gmail.com>
2023-08-16 18:19:13 +0600

    Border and Rolling

--- core/src/com/mygdx/game/screens/MainGameScreen.java
+++ core/src/com/mygdx/game/screens/MainGameScreen.java
@@ -10,13 +10,15 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.mygdx.game.MyGdxGame;
 
 public class MainGameScreen implements Screen {
-    public static final float SPEED = 120;
+    public static final float SPEED = 200;
 
     public static final float SHIP_ANIMATION_SPEED = 0.5f;
     public static final int SHIP_WIDTH_PIXEL = 17;
     public static final int SHIP_HEIGHT_PIXEL = 32;
     public static final int SHIP_WIDTH = SHIP_WIDTH_PIXEL * 3;
     public static final int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL * 3;
+    public static final float ROLL_TIMER_SWITCH_TIME =0.15f;
+
 
 
     Animation[] rolls;
@@ -24,6 +26,7 @@ public class MainGameScreen implements Screen {
     float y;
     int roll;
     float stateTime;
+    float rollTimer;
 
     MyGdxGame game;
 
@@ -33,11 +36,16 @@ public class MainGameScreen implements Screen {
         x = (float) MyGdxGame.Width / 2 - (float) SHIP_WIDTH / 2;
 
         roll = 2;
+        rollTimer=0;
         rolls = new Animation[5];
 
         TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), SHIP_WIDTH_PIXEL, SHIP_HEIGHT_PIXEL);
 
-        rolls[roll] = new Animation(SHIP_ANIMATION_SPEED,rollSpriteSheet[0]);
+        rolls[0] = new Animation( SHIP_ANIMATION_SPEED, rollSpriteSheet[2]);
+        rolls[1] = new Animation( SHIP_ANIMATION_SPEED, rollSpriteSheet[1]);
+        rolls[2] = new Animation( SHIP_ANIMATION_SPEED, rollSpriteSheet[0]);
+        rolls[3] = new Animation( SHIP_ANIMATION_SPEED, rollSpriteSheet[3]);
+        rolls[4] = new Animation( SHIP_ANIMATION_SPEED, rollSpriteSheet[4]);
     }
     @Override
     public void show() {
@@ -48,14 +56,42 @@ public class MainGameScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            x+=SPEED*Gdx.graphics.getDeltaTime();
-
-        }
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
             x-=SPEED*Gdx.graphics.getDeltaTime();
 
+            if(x<0){
+                x=0;
+            }
+            rollTimer-=Gdx.graphics.getDeltaTime();
+            if(Math.abs(rollTimer)>ROLL_TIMER_SWITCH_TIME)
+            {
+                rollTimer=0;
+                roll--;
+                if(roll<0){
+                    roll=0;
+                }
+            }
+
         }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            x+=SPEED*Gdx.graphics.getDeltaTime();
+            if(x+SHIP_WIDTH >Gdx.graphics.getWidth())
+                x=Gdx.graphics.getWidth()-SHIP_WIDTH;
+
+            rollTimer+=Gdx.graphics.getDeltaTime();
+            if(Math.abs(rollTimer)>ROLL_TIMER_SWITCH_TIME)
+            {
+                rollTimer=0;
+                roll++;
+
+                if(roll>4){
+                    roll=4;
+                }
+            }
+
+        }
+
         stateTime +=delta;
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
